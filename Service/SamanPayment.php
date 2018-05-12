5b3ee4156a1b17e8ec7af7748278e5a15419c452
mosi <mooosti.maxx@gmail.com>
2018-05-13 03:07:09 +0430

    add form support

--- Service/SamanPayment.php
+++ Service/SamanPayment.php
@@ -3,6 +3,8 @@
 namespace EricomGroup\SamanPaymentBundle\Service;
 
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
 /**
  * Class SamanPayment
@@ -13,6 +15,10 @@ class SamanPayment
     const FORM_ACTION  = 'https://sep.shaparak.ir/Payment.aspx';
     const WEB_METHOD_URL = 'https://verify.sep.ir/Payments/ReferencePayment.asmx?wsdl';
 
+	/**
+	 * @var
+	 */
+	private $container;
     /**
      * @var mixed
      */
@@ -118,6 +124,7 @@ class SamanPayment
      */
     public function __construct(ContainerInterface $container)
 	{
+		$this->container = $container;
 		$this->config = $container->getParameter('saman_payment.config');
 		$this->bankRate = 1;
 
@@ -298,5 +305,24 @@ class SamanPayment
 			'autoSubmit' => $this->isAutoSubmit,
 			'submitText' => $this->submitText
 		];
+	}
+
+	/**
+	 * @return \Symfony\Component\Form\FormBuilderInterface
+	 */
+	public function getForm()
+	{
+		$form = $this->container->get('form.factory')->createNamedBuilder(null, FormType::class, null, [
+			'action' => SamanPayment::FORM_ACTION,
+			'csrf_protection' => false,
+			'method' => 'post'
+		])->add('merchantId', HiddenType::class, [
+			'data' => $this->merchantId
+		])->add('resNum', HiddenType::class, [
+			'data' => $this->paymentId
+		])->add('redirectUrl', HiddenType::class, [
+			'data' => $this->redirectUrl
+		]);
+		return $form;
 	}
 }
